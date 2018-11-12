a819affb3c12f5153ccf224f9d2e8253a18f9f35
bogdan <bogdya.linnik@gmail.com>
2018-11-12 11:21:52 +0200

    Add post reducer

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,42 @@
+import axiosClient from '../axiosClient';
+import { FETCH_POSTS, FETCH_POST, NEW_POST, UPDATE_POST, DELETE_POST } from '../actions/types'
+
+export const fetchPosts = (categoryId) => dispatch => {
+  axiosClient.get(`categories/${categoryId}.json`).then((response) => {
+    dispatch({
+      type: FETCH_POSTS,
+      category: response.data.category,
+      posts: response.data.posts
+    })
+  });
+}
+
+export const createPost = (postData, config) => dispatch => {
+  axiosClient.post(`posts.json`, postData, config)
+             .then((response) => {
+    dispatch({
+      type: NEW_POST,
+      payload: response.data.post
+    })
+  });
+}
+
+export const updatePost = (postId, postData, config) => dispatch => {
+  axiosClient.patch(`posts/${postId}.json`, postData, config)
+             .then((response) => {
+    dispatch({
+      type: UPDATE_POST,
+      payload: response.data.post
+    })
+  });
+}
+
+export const deletePost = (postData) => dispatch => {
+  axiosClient.delete(`posts/${postData.id}.json`)
+             .then((response) => {
+    dispatch({
+      type: DELETE_POST,
+      payload: postData
+    })
+  })
+}
---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,45 @@
+import { FETCH_POSTS, FETCH_POST, NEW_POST, UPDATE_POST, DELETE_POST } from '../actions/types'
+
+const initialState = {
+  type: '',
+  category: {},
+  items: []
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POSTS:
+      return {
+        ...state,
+        type: action.type,
+        category: action.category,
+        items: action.posts
+      }
+    case FETCH_POST:
+      return {
+        ...state,
+        type: action.type,
+        item: action.payload
+      }
+    case NEW_POST:
+      return {
+        ...state,
+        type: action.type,
+        newItem: action.payload
+      }
+    case UPDATE_POST:
+      return {
+        ...state,
+        type: action.type,
+        updatedItem: action.payload
+      }
+    case DELETE_POST:
+      return {
+        ...state,
+        type: action.type,
+        deletedItem: action.payload
+      }
+    default:
+      return state;
+  }
+}
